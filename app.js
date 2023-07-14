c2320e6eeda32648e88f0d20bc4a10206dac4793
Brian Fiset <fisetbrian@gmail.com>
2023-07-13 22:54:47 -0400

    Add random colors and grid size changer

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const gridSize = document.querySelector('.grid-size');
+const container = document.querySelector('.etch-container');
+const gridSizeText = document.querySelector('.grid-size-text');
+let mouseDown = false;
+let color = 'black';
+const selectColor = document.querySelector('#colors');
+
 function createGridItems(element){
     const container = document.querySelector(element);
     const item = document.createElement('div');
@@ -6,7 +13,6 @@ function createGridItems(element){
 };
 
 function createGridLines(element){
-    const container = document.querySelector('.etch-container');
     const itemLine = document.createElement('div');
     itemLine.classList.add('item-line');
     itemLine.classList.add(element);
@@ -14,37 +20,72 @@ function createGridLines(element){
 };
 
 function createGrid(number){
+    deleteGrid()
     for(let i = 1; i <= number; i++) {
         createGridLines(`line-${i}`);
         for(let j = 1; j <= number; j++) {
             createGridItems(`.line-${i}`);
         }
     }
+    coloringGrid()
+};
+
+function deleteGrid(){
+    container.innerText = ''
 }
 
-createGrid(16)
+createGrid(16);
 
-const gridItem = document.querySelectorAll('.item');
-let mouseDown = false;
 
-gridItem.forEach(item => item.addEventListener('mousedown', (e) => {
-    changeColor(e)
-    mouseDown = true;
-    itemHover()
-}));
+function coloringGrid(){
+    const gridItem = document.querySelectorAll('.item');
+    gridItem.forEach(item => item.addEventListener('mousedown', (e) => {
+        changeColor(e);
+        mouseDown = true;
+        itemHover(gridItem);
+    }));
 
-gridItem.forEach(item => item.addEventListener('mouseup', () => {
-    mouseDown = false;
-    itemHover()
-}));
+    gridItem.forEach(item => item.addEventListener('mouseup', () => {
+        mouseDown = false;
+        itemHover(gridItem);
+    }));
+    container.addEventListener('mouseleave', () => {
+        mouseDown = false;
+        itemHover(gridItem);
+    });
+}
 
-function itemHover() {
+function itemHover(gridItem) {
     if (mouseDown === true) {
         gridItem.forEach(item => item.addEventListener('mouseover', changeColor));
     } else if (mouseDown === false) {
         gridItem.forEach(item => item.removeEventListener('mouseover',changeColor));
     };
 }
+
+function randomColor() {
+    let color = `#${Math.floor(Math.random()*16777215).toString(16)}`
+    return color
+}
+
 function changeColor(event) {
-    event.target.style = `background-color: black;`
+    if(color === 'random') {
+        event.target.style = `background-color: ${randomColor()};`
+    } else if(color === 'black') {
+        event.target.style = `background-color: black;`
+    }
 };
+
+gridSize.addEventListener('change', () => {
+     changeText(gridSizeText, `${gridSize.value}X${gridSize.value}`);
+     createGrid(gridSize.value);
+});
+
+function changeText(content, text){
+    content.innerText = text;
+};
+
+
+selectColor.addEventListener('change', () => {
+    color = selectColor.value;
+});
